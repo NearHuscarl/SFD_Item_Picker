bd91a967b8ab13e906006c24fa34f73a87483279
NearHuscarl <near.huscarl@gmail.com>
2021-02-05 23:58:55 +0700

    fix eslint error

--- src/app/pages/home/HomePage.tsx
+++ src/app/pages/home/HomePage.tsx
@@ -36,6 +36,7 @@ function ItemAutocomplete(props: ItemAutocompleteProps) {
         onChange(opposite);
       }
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentGender]);
 
   return (
